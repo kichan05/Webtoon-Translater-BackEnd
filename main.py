9dca5d7fa9115cbe172c752764b0c4d36402741e
바키찬 <ckstmznf@naver.com>
2023-07-16 13:57:40 +0900

    add:package.txt

--- main.py
+++ main.py
@@ -9,12 +9,19 @@ from webtoonTranslater import WebtoonTranslater
 app = FastAPI()
 webtoonTranslater = WebtoonTranslater(config.CLOVA_OCR_API_KEY)
 
+
 @app.get("/")
 def root():
     return "Hello World"
 
+
+@app.get("/test")
+def test():
+    timeStamp = datetime.now().strftime("%Y-%m-%d %H.%M.%S")
+    return {"result": "OK", "timeStamp": timeStamp}
+
 @app.post("/imageOcr")
-async def root(fileList: list[UploadFile]):
+async def imageOcr(fileList: list[UploadFile]):
     timeStamp = datetime.now().strftime("%Y-%m-%d %H.%M.%S")
 
     image_list = []
@@ -31,6 +38,17 @@ async def root(fileList: list[UploadFile]):
     ocr_result = webtoonTranslater.imageOCR(image_path)
 
     return {"timeStamp": timeStamp, "ocr": ocr_result}
+
+
+@app.post("/imageUpload")
+async def imageUpload(fileList: list[UploadFile]):
+    for file in fileList:
+        content = await file.read()
+        image = Image.open(BytesIO(content))
+        image.show()
+
+    return {"result": "OK"}
+
 
 @app.get("/image/{image_name}")
 def getImage(image_name):
